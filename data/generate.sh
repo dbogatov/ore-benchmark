4792dc533318b92dd94ddc07f8ddd4d56bdbc18f
Dmytro Bogatov <dmytro@dbogatov.org>
2018-04-30 04:05:51 -0400

    Fix CI.

--- data/generate.sh
+++ data/generate.sh
@@ -7,18 +7,22 @@ shopt -s globstar
 cd "${0%/*}"
 CWD=$(pwd)
 
-usage() { echo "Usage: $0 [-m <number> -s <number>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-m <number> -s <number> -n]" 1>&2; exit 1; }
 
 SEED=$RANDOM
 MAX=100
+BUILD=true
 
-while getopts "m:s:" o; do
+while getopts "m:s:n" o; do
 	case "${o}" in
 		m)
 			MAX=${OPTARG}
 			;;
 		s)
 			SEED=${OPTARG}
+			;;
+		n)
+			BUILD=false
 			;;
 		*)
 			usage
@@ -27,15 +31,18 @@ while getopts "m:s:" o; do
 done
 shift $((OPTIND-1))
 
-dotnet build -c release ../tools/data-gen/
+if [ "$BUILD" == true ];
+then
+	dotnet build -c release ../tools/data-gen/ -o dist/
+fi
 
 set -x # echo ON
-dotnet ../tools/data-gen/bin/release/netcoreapp2.0/data-gen.dll --dataset --count $MAX --max $MAX --seed $SEED > dataset.txt
+dotnet ../tools/data-gen/dist/data-gen.dll --dataset --count $MAX --max $MAX --seed $SEED > dataset.txt
 
 queries=( exact range update delete )
 for query in "${queries[@]}"
 do
-	dotnet ../tools/data-gen/bin/release/netcoreapp2.0/data-gen.dll --queries-type $query --count $MAX --max $MAX --seed $SEED > $query-queries.txt
+	dotnet ../tools/data-gen/dist/data-gen.dll --queries-type $query --count $MAX --max $MAX --seed $SEED > $query-queries.txt
 done
 
 echo "Done!"
