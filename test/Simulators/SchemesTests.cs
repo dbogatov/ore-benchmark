08e5e15e006dc2d8de642ce74bc7ec10902e24c6
Dmytro Bogatov <dmytro@dbogatov.org>
2018-06-07 02:27:48 -0400

    Fix tests.

--- test/Simulators/SchemesTests.cs
+++ test/Simulators/SchemesTests.cs
@@ -6,6 +6,7 @@ using ORESchemes.Shared;
 using DataStructures.BPlusTree;
 using System.Collections.Generic;
 using Simulation.PureSchemes;
+using ORESchemes.LewiORE;
 
 namespace Test.Simulators
 {
@@ -15,19 +16,41 @@ namespace Test.Simulators
 		[Fact]
 		public void SimulatorTest()
 		{
-			var max = 1000000;
+			byte[] entropy = new byte[256 / 8];
+			new Random(123456).NextBytes(entropy);
+
+			var max = 100;
 			var dataset =
 				Enumerable
 					.Range(-max, max)
 					.ToList();
 
-			var simulator = new Simulator<long>(dataset, new NoEncryptionScheme());
+			var simulator = new Simulator<Ciphertext>(dataset, new LewiOREScheme(16, entropy));
 			var report = simulator.Simulate();
 
-			foreach (var subreport in new List<Report.Subreport> { report.Encryptions, report.Decryptions, report.Comparisons })
+			var subreports = new List<Report.Subreport> { report.Encryptions, report.Decryptions, report.Comparisons };
+
+			foreach (var subreport in subreports)
 			{
 				Assert.NotEqual(0, subreport.OperationsNumber);
 				Assert.NotEqual(new TimeSpan(0).Ticks, subreport.ObservedTime.Ticks);
+				Assert.NotEqual(0, subreport.PurePrimitiveOperations.Values.Sum());
+				Assert.NotEqual(0, subreport.TotalPrimitiveOperations.Values.Sum());
+			}
+
+			var descriptions = new List<string> {
+				report.ToString(),
+				report.ToConciseString()
+			};
+
+			foreach (var description in descriptions)
+			{
+				foreach (var subreport in subreports)
+				{
+					Assert.Contains(subreport.OperationsNumber.ToString(), description);
+					Assert.Contains(subreport.ObservedTime.TotalMilliseconds.ToString(), description);
+					Assert.Contains(subreport.CPUTime.TotalMilliseconds.ToString(), description);
+				}
 			}
 		}
 	}
