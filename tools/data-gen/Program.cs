274e761a8c66050e04817491336abae603812590
Dmytro Bogatov <dmytro@dbogatov.org>
2018-09-07 15:48:40 -0400

    Add forest data set.

--- tools/data-gen/Program.cs
+++ tools/data-gen/Program.cs
@@ -44,6 +44,30 @@ namespace DataGen
 		public string Status;
 	}
 
+	[DelimitedRecord(",")]
+	public class Forest
+	{
+		public int Elevation;
+		public int Aspect;
+		public int Slope;
+		public int HorizontalDistanceToHydrology;
+		public int VerticalDistanceToHydrology;
+		public int HorizontalDistanceToRoadways;
+		public int Hillshade9am;
+		public int HillshadeNoon;
+		public int Hillshade3pm;
+		public int HorizontalDistanceToFirePoints;
+
+		[FieldArrayLength(4)]
+		public int[] WildernessArea;
+
+		[FieldArrayLength(40)]
+		public int[] SoilType;
+
+		public int CoverType;
+
+	}
+
 	[HelpOption("-?|-h|--help")]
 	[Command(Name = "data-gen", Description = "Data generation utility", ThrowOnUnexpectedArgument = true)]
 	class Program
@@ -72,6 +96,12 @@ namespace DataGen
 		[Range(1, double.MaxValue)]
 		[Option("--zipf-param <number>", Description = "Defines the Zipf distribution parameter. Default 1.5.")]
 		public double ZipfParam { get; } = 1.5;
+
+		[Option("--employees-url <string>", Description = "If Employees type is requested, this is a CSV file's URL.")]
+		public string EmployeesUrl { get; } // https://vadim-dolores-space.nyc3.digitaloceanspaces.com/public/state-of-california-2017.csv
+
+		[Option("--forest-url <string>", Description = "If Forest type is requested, this is a CSV file's URL.")]
+		public string ForestUrl { get; } // https://vadim-dolores-space.nyc3.digitaloceanspaces.com/public/covtype.data
 
 		[Range(10, 1000000)]
 		[Option("--query-size <number>", Description = "Size of the query set to generate. Default 1000.")]
@@ -120,9 +150,13 @@ namespace DataGen
 					}
 					break;
 				case Type.Employees:
+					if (EmployeesUrl == null)
+					{
+						throw new ArgumentException("--employees-url must be set to use Employees data set.");
+					}
 					using (var client = new HttpClient())
 					{
-						var responseString = await client.GetStringAsync("https://vadim-dolores-space.nyc3.digitaloceanspaces.com/public/state-of-california-2017.csv");
+						var responseString = await client.GetStringAsync(EmployeesUrl);
 
 						var set = new HashSet<int>();
 						var tmpList = new List<int>();
@@ -153,8 +187,42 @@ namespace DataGen
 					}
 					break;
 				case Type.Forest:
-				// TODO
-				// https://archive.ics.uci.edu/ml/datasets/Covertype
+					if (ForestUrl == null)
+					{
+						throw new ArgumentException("--forest-url must be set to use Forest data set.");
+					}
+					using (var client = new HttpClient())
+					{
+						var responseString = await client.GetStringAsync(ForestUrl);
+
+						var set = new HashSet<int>();
+						var tmpList = new List<int>();
+						var engine = new FileHelperAsyncEngine<Forest>();
+
+						using (engine.BeginReadString(responseString))
+						{
+							foreach (var forest in engine)
+							{
+								tmpList.Add(forest.Elevation);
+							}
+						}
+
+						var count = tmpList.Count();
+						for (int i = 0; i < DataSize; i++)
+						{
+							if (!set.Add(generator.Next(0, count)))
+							{
+								i--;
+								continue;
+							}
+						}
+
+						foreach (var index in set)
+						{
+							data.Add(tmpList[index]);
+						}
+					}
+					break;
 				default:
 					throw new ArgumentException();
 			}
