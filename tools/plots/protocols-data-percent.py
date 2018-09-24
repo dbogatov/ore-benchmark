fb8c7d55e73cb4a996865254b6ce1e2f3006ceed
Dmytro Bogatov <dmytro@dbogatov.org>
2018-09-23 20:49:31 -0400

    Merge BCLO, CLWW and FH-OPE in one plot.

--- tools/plots/protocols-data-percent.py
+++ tools/plots/protocols-data-percent.py
@@ -9,9 +9,9 @@ import numpy as np
 
 value = str(sys.argv[1])
 
-names = ('No encryption', 'BCLO', 'CLWW', 'Lewi-Wu', 'FH-OPE', 'CLOZ',
+names = ('No encryption', 'BCLO, CLWW\nFH-OPE', 'Lewi-Wu', 'CLOZ',
          'Kerschbaum', 'POPE cold', 'POPE warm')
-N = len(names)
+N = len(names) + 2
 
 percent5 = []
 percent10 = []
@@ -24,19 +24,24 @@ with open("./data/protocols-data-percent-{0}.txt".format(value)) as fp:
     counter = 0
     while line:
         if counter < N:
-            percent5.append(int(line.strip()))
+            if counter % N != 2 and counter % N != 4:
+                percent5.append(int(line.strip()))
         elif counter < 2 * N:
-            percent10.append(int(line.strip()))
+            if counter % N != 2 and counter % N != 4:
+                percent10.append(int(line.strip()))
         elif counter < 3 * N:
-            percent20.append(int(line.strip()))
+            if counter % N != 2 and counter % N != 4:
+                percent20.append(int(line.strip()))
         elif counter < 4 * N:
-            percent50.append(int(line.strip()))
+            if counter % N != 2 and counter % N != 4:
+                percent50.append(int(line.strip()))
         elif counter < 5 * N:
-            percent100.append(int(line.strip()))
+            if counter % N != 2 and counter % N != 4:
+                percent100.append(int(line.strip()))
         line = fp.readline()
         counter += 1
 
-ind = np.arange(N)
+ind = np.arange(N - 2)
 width = 1.0 / 6
 
 alpha = 0.5
@@ -46,34 +51,34 @@ plt.style.use('grayscale')
 f, (ax, ax2) = plt.subplots(2, 1, sharex=True)
 
 for axis in [ax, ax2]:
-	axis.bar(ind, percent5, width, alpha=alpha, edgecolor="black", label='5%')
-	axis.bar(ind + width, percent10, width, alpha=alpha,
-			edgecolor="black", label='10%')
-	axis.bar(ind + 2 * width, percent20, width, alpha=alpha,
-			edgecolor="black", label='20%')
-	axis.bar(ind + 3 * width, percent50, width, alpha=alpha,
-			edgecolor="black", label='50%')
-	axis.bar(ind + 4 * width, percent100, width, alpha=alpha,
-			edgecolor="black", label='100%')
+    axis.bar(ind, percent5, width, alpha=alpha, edgecolor="black", label='5%')
+    axis.bar(ind + width, percent10, width, alpha=alpha,
+            edgecolor="black", label='10%')
+    axis.bar(ind + 2 * width, percent20, width, alpha=alpha,
+            edgecolor="black", label='20%')
+    axis.bar(ind + 3 * width, percent50, width, alpha=alpha,
+            edgecolor="black", label='50%')
+    axis.bar(ind + 4 * width, percent100, width, alpha=alpha,
+            edgecolor="black", label='100%')
 
 if value == "cios":
-	ax.set_ylim(20, 500)  # outliers only
-	ax2.set_ylim(0, 8.5)  # most of the data
+    ax.set_ylim(20, 500)  # outliers only
+    ax2.set_ylim(0, 8.5)  # most of the data
 if value == "cvol":
-	ax.set_ylim(29, 42)  # outliers only
-	ax2.set_ylim(0, 5.5)  # most of the data
+    ax.set_ylim(29, 42)  # outliers only
+    ax2.set_ylim(0, 5.5)  # most of the data
 elif value == "csize":
-	ax.set_ylim(315, 570)  # outliers only
-	ax2.set_ylim(0, 55)  # most of the data
+    ax.set_ylim(315, 570)  # outliers only
+    ax2.set_ylim(0, 55)  # most of the data
 elif value == "qios":
-	ax.set_ylim(150, 2200)  # outliers only
-	ax2.set_ylim(0, 105)  # most of the data
+    ax.set_ylim(150, 2200)  # outliers only
+    ax2.set_ylim(0, 105)  # most of the data
 elif value == "qvol":
-	ax.set_ylim(18000, 550000)  # outliers only
-	ax2.set_ylim(0, 1200)  # most of the data
+    ax.set_ylim(18000, 550000)  # outliers only
+    ax2.set_ylim(0, 1200)  # most of the data
 elif value == "qsize":
-	ax.set_ylim(40001, 1000000)  # outliers only
-	ax2.set_ylim(0, 40050)  # most of the data
+    ax.set_ylim(40001, 1000000)  # outliers only
+    ax2.set_ylim(0, 40050)  # most of the data
 
 
 ax.spines['bottom'].set_visible(False)
@@ -95,11 +100,11 @@ ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
 plt.xticks(ind + 2 * width, names, rotation=45)
 
 for axis in [ax, ax2]:
-	axis.grid(linestyle='dotted', alpha=0.5)
-	axis.yaxis.set_major_locator(MaxNLocator(integer=True))
-	axis.get_yaxis().set_major_formatter(
-		mpl.ticker.FuncFormatter(lambda x, p: format(int(x), ','))
-	)
+    axis.grid(linestyle='dotted', alpha=0.5)
+    axis.yaxis.set_major_locator(MaxNLocator(integer=True))
+    axis.get_yaxis().set_major_formatter(
+        mpl.ticker.FuncFormatter(lambda x, p: format(int(x), ','))
+    )
 
 ax.legend(loc='upper left')
 
