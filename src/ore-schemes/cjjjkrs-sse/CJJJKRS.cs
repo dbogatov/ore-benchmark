e975fe8d5398f1bc651cd0bb9b4ce18d868d7fcc
Dmytro Bogatov <dmytro@dbogatov.org>
2019-01-15 02:40:30 -0500

    Implemented CJJJKRS (bas), not run not tested.

---
+++ src/ore-schemes/cjjjkrs-sse/CJJJKRS.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ORESchemes.Shared;
+using ORESchemes.Shared.Primitives;
+using ORESchemes.Shared.Primitives.PRF;
+using ORESchemes.Shared.Primitives.PRG;
+using ORESchemes.Shared.Primitives.Symmetric;
+
+namespace ORESchemes.CJJJKRS
+{
+	/// <summary>
+	/// A byteable keyword for SSE
+	/// </summary>
+	public interface IWord : IByteable { }
+	/// <summary>
+	/// A byteable index for SSE
+	/// </summary>
+	public interface IIndex : IByteable { }
+
+	/// <summary>
+	/// A namespace wrapper that ensures that the same generic types are provided
+	/// to Client and Server
+	/// </summary>
+	/// <typeparam name="W">Word type</typeparam>
+	/// <typeparam name="I">Index type</typeparam>
+	public static class CJJKRS<W, I>
+		where W : IWord
+		where I : IIndex
+	{
+		/// <summary>
+		/// Convenience wrapper class to wrap around TSet types
+		/// </summary>
+		/// <typeparam name="T">Wrapped type</typeparam>
+		public abstract class Wrapper<T>
+		{
+			public T Value { get; set; }
+		}
+
+		public class Database : Wrapper<Dictionary<byte[], byte[]>>
+		{
+			public int Size { get => 8 * Value.Sum(kvp => kvp.Key.Length + kvp.Value.Length); }
+		}
+
+		public class Token : Wrapper<(byte[] k1, byte[] k2)>
+		{
+			public int Size { get => 8 * (Value.k1.Length + Value.k2.Length); }
+		}
+
+		public class Client : EventHandlers
+		{
+			private readonly IPRG G;
+			private readonly IPRF F;
+			private readonly ISymmetric E;
+
+			public Client(byte[] entropy = null)
+			{
+				G = new PRGFactory(entropy).GetPrimitive();
+				F = new PRFFactory().GetPrimitive();
+				E = new SymmetricFactory().GetPrimitive();
+
+				SubscribePrimitive(G);
+				SubscribePrimitive(F);
+				SubscribePrimitive(E);
+			}
+
+			public (Database, byte[]) Setup(Dictionary<W, I[]> input)
+			{
+				var key = G.GetBytes(256 / 8);
+				var result = new Dictionary<byte[], byte[]>();
+
+				foreach (var wordIndices in input)
+				{
+					var word = wordIndices.Key;
+					var indices = wordIndices.Value;
+
+					var k1 = F.PRF(key.Take(128 / 8).ToArray(), word.ToBytes());
+					var k2 = F.PRF(key.Skip(128 / 8).ToArray(), word.ToBytes());
+
+					for (int c = 0; c < indices.Length; c++)
+					{
+						var l = F.PRF(k1, BitConverter.GetBytes(c));
+						var d = E.Encrypt(k2, indices[c].ToBytes());
+						result.Add(l, d);
+					}
+				}
+
+				return (new Database { Value = result }, key);
+			}
+
+			public Token Trapdoor(W keyword, byte[] key)
+				=> new Token
+				{
+					Value = (
+						k1: F.PRF(key.Take(128 / 8).ToArray(), keyword.ToBytes()),
+						k2: F.PRF(key.Skip(128 / 8).ToArray(), keyword.ToBytes())
+					)
+				};
+		}
+
+		public class Server : EventHandlers
+		{
+			private readonly Database _database;
+			private readonly IPRF F;
+			private readonly ISymmetric E;
+
+			/// <summary>
+			/// I/O page size in bits.
+			/// If set, NodeVisited event will be fired.
+			/// </summary>
+			public int? PageSize { private get; set; }
+
+			public Server(Database database)
+			{
+				_database = database;
+
+				F = new PRFFactory().GetPrimitive();
+				E = new SymmetricFactory().GetPrimitive();
+
+				SubscribePrimitive(F);
+				SubscribePrimitive(E);
+			}
+
+			public I[] Search(Token token, Func<byte[], I> decode)
+			{
+				var c = 0;
+				var result = new List<I>();
+
+				while (true)
+				{
+					var dictKey = F.PRF(token.Value.k1, BitConverter.GetBytes(c));
+					if (_database.Value.ContainsKey(dictKey))
+					{
+						var d = _database.Value[dictKey];
+						var id = E.Decrypt(token.Value.k2, d);
+						result.Add(decode(id));
+					}
+					else
+					{
+						break;
+					}
+				}
+
+				return result.ToArray();
+			}
+		}
+
+	}
+}
